7bfd47cb7b176090fbbe46b42665e8a022fce871
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-05-29 17:42:37 +0200

    Uniform from script + Get data from the shader

--- ShaderCourse(GameDevTV)/Shaders101/scripts/shader_101.gd
+++ ShaderCourse(GameDevTV)/Shaders101/scripts/shader_101.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+@onready var sprite = $Sprite2D
+
 func _process(delta):
-	print(str(Time.get_ticks_msec() / 1000.0))
+	if Input.is_action_just_pressed("set_speed"):
+		var new_speed = 10
+		sprite.material.set_shader_parameter("speed", new_speed)
+		var new_color = Vector4(randf(), randf(), randf(), 1.0)
+		sprite.material.set_shader_parameter("color_example", new_color)
+		
+	# Get data from the shader
+	var color = sprite.material.get_shader_parameter("color_example")
+	print(color)
