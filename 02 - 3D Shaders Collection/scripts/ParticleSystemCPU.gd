0098c946990ed5705c63dada135eb370bbab768b
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-07-16 19:14:55 +0200

    delta add

--- 02 - 3D Shaders Collection/scripts/ParticleSystemCPU.gd
+++ 02 - 3D Shaders Collection/scripts/ParticleSystemCPU.gd
@@ -23,7 +23,7 @@ func _ready():
 	shader = _2d_particles_cpu.get_active_material(0)
 	for n in number_of_particals:
 		var p = Particle.new()
-		p.p_pos = source + Vector2(generate_random_float(), generate_random_float())
+		p.p_pos = source
 		p.p_radius = radius
 		particles.append(p)
 	shader.set_shader_parameter("size", number_of_particals)
@@ -33,9 +33,9 @@ func _ready():
 func _process(delta):
 	var array_to_shader: Array[float] = [number_of_particals * 3] 
 	for n in number_of_particals:
-		particles[n].p_pos += Vector2(generate_random_float(), generate_random_float())
-		array_to_shader.append(particles[n].p_pos.x)
+		particles[n].p_pos += Vector2(generate_random_float(), generate_random_float_x()) * 10 * delta
 		array_to_shader.append(particles[n].p_pos.y)
+		array_to_shader.append(particles[n].p_pos.x)
 		array_to_shader.append(particles[n].p_radius)
 	
 	shader.set_shader_parameter("array", array_to_shader);
@@ -43,4 +43,9 @@ func _process(delta):
 func generate_random_float() -> float:
 	var min_value: float = 0.01
 	var max_value: float = 0.05
+	return randf_range(min_value, max_value)
+	
+func generate_random_float_x() -> float:
+	var min_value: float = -0.1
+	var max_value: float = 0.1
 	return randf_range(min_value, max_value)
